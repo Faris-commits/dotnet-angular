623b960b9aa3409ef7684aab65e9c873246f168c
faris.omerbasic@htecgroup.com <faris.omerbasic@htecgroup.com>
2025-05-02 23:59:58 +0200

    Section 14: Added likes feature, you can like or be liked

--- API/Helpers/UserParams.cs
+++ API/Helpers/UserParams.cs
@@ -1,17 +1,10 @@
+using API.Helpers;
+
 namespace API;
 
-public class UserParams
+public class UserParams : PaginationParams
 {
-    private const int MaxPageSize = 50;
-
-    public int PageNumber { get; set; } = 1;
-
-    public int _pageSize = 10;
-    public int PageSize
-    {
-        get => _pageSize;
-        set => _pageSize = (value > MaxPageSize) ? MaxPageSize : value;
-    }
+ 
 
     public string? Gender { get; set; }
 
